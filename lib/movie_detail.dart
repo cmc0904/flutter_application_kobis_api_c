9c67964abe23b564ef6b01ebab8e936ded509fb5
anscks <anscks2350@naver.com>
2023-06-14 18:47:37 +0900

    adsa

--- lib/movie_detail.dart
+++ lib/movie_detail.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_kobis_api/kobis_api.dart';
 
 class MovieDetail extends StatelessWidget {
   final String movieCd;
-  const MovieDetail({super.key, required this.movieCd});
+  MovieDetail({super.key, required this.movieCd});
+  final kobisApi = KobisApi(apiKey: '5f591c41f4efbb8d272b60244907e9bd');
 
   @override
   Widget build(BuildContext context) {

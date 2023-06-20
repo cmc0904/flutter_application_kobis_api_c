5bdf874f018356a5cedf57d35d8e77cd3c223ad7
anscks <anscks2350@naver.com>
2023-06-20 18:25:28 +0900

    saasas

--- lib/movie_detail.dart
+++ lib/movie_detail.dart
@@ -3,159 +3,217 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_kobis_api/kobis_api.dart';
 import 'package:accordion/accordion.dart';
 
-
 class MovieDetail extends StatelessWidget {
   final String movieCd;
   MovieDetail({super.key, required this.movieCd});
   final kobisApi = KobisApi(apiKey: '5f591c41f4efbb8d272b60244907e9bd');
 
-  final _headerStyle = const TextStyle(
-      color: Color(0xffffffff), fontSize: 15, fontWeight: FontWeight.bold);
-  final _contentStyleHeader = const TextStyle(
-      color: Color(0xff999999), fontSize: 14, fontWeight: FontWeight.w700);
-  final _contentStyle = const TextStyle(
-      color: Color(0xff999999), fontSize: 14, fontWeight: FontWeight.normal);
-  final _loremIpsum =
-      '''Lorem ipsum is typically a corrupted version of 'De finibus bonorum et malorum', a 1st century BC text by the Roman statesman and philosopher Cicero, with words altered, added, and removed to make it nonsensical and improper Latin.''';
-
-
   @override
   Widget build(BuildContext context) {
-    var movie = kobisApi.getMovieDetail(movieCd: movieCd);
-
     return Scaffold(
       appBar: AppBar(title: Text("영화 상세 정보"), centerTitle: true),
       body: FutureBuilder(
-        future: movie,
-        builder: (context, snapshot) {
-          if(snapshot.hasData) {
-            var moviesData = snapshot.data;
+          future: kobisApi.getMovieDetail(movieCd: movieCd),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var moviesData = snapshot.data;
 
-            return Column(
-              children: [
-
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Text("영화 제목 : ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                      Text(moviesData?['movieNm'], style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                    ],
+              return Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "영화 제목 : ",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          moviesData?['movieNm'],
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Text("개봉일 : ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                      Text(moviesData?['movieNmEn'], style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "개봉일 : ",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          moviesData?['movieNmEn'],
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Text("시청 연령 제한 : ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                      Text(moviesData?['audits'][0]["watchGradeNm"], style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "시청 연령 제한 : ",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          moviesData?['audits'][0]["watchGradeNm"],
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Text("개봉일 : ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                      Text(moviesData?['openDt'], style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          "개봉일 : ",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          moviesData?['openDt'],
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-
-                Divider(),
-
-                Text("추가정보", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-
-                Accordion(
-          maxOpenSections: 2,
-          headerBackgroundColorOpened: Colors.black54,
-          scaleWhenAnimating: true,
-          openAndCloseAnimation: true,
-          headerPadding:
-                  const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
-          sectionOpeningHapticFeedback: SectionHapticFeedback.heavy,
-          sectionClosingHapticFeedback: SectionHapticFeedback.light,
-          children: [
-                AccordionSection(
-                  isOpen: false,
-                  leftIcon: const Icon(Icons.build, color: Colors.white),
-                  header: Text('제작 참여 회사', style: _headerStyle),
-                  content: DataTable(
-                    sortAscending: true,
-                    sortColumnIndex: 1,
-                    dataRowHeight: 40,
-                    showBottomBorder: false,
-                    columns: [
-                      DataColumn(
-                          label: Text('회사', style: _contentStyleHeader),),
-                      DataColumn(
-                          label: Text('역할', style: _contentStyleHeader),),
-                    ],
-                    rows: [
-                      for (var i in moviesData?['companys'])
-                      DataRow(
-                        cells: [
-                          DataCell(Text(i['companyNm'],
-                              style: _contentStyle, textAlign: TextAlign.right)),
-                          DataCell(Text(i['companyPartNm'],
-                              style: _contentStyle, textAlign: TextAlign.right))
-                        ],
+                  Divider(),
+                  Text(
+                    "추가정보",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Accordion(
+                    maxOpenSections: 2,
+                    headerBackgroundColorOpened: Colors.black54,
+                    scaleWhenAnimating: true,
+                    openAndCloseAnimation: true,
+                    headerPadding:
+                        const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+                    sectionOpeningHapticFeedback: SectionHapticFeedback.heavy,
+                    sectionClosingHapticFeedback: SectionHapticFeedback.light,
+                    children: [
+                      AccordionSection(
+                        isOpen: false,
+                        leftIcon: const Icon(Icons.build, color: Colors.white),
+                        header: Text('제작 참여 회사',
+                            style: TextStyle(
+                                color: Color(0xffffffff),
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold)),
+                        content: DataTable(
+                          sortAscending: true,
+                          sortColumnIndex: 1,
+                          dataRowHeight: 40,
+                          showBottomBorder: false,
+                          columns: [
+                            DataColumn(
+                              label: Text('회사',
+                                  style: TextStyle(
+                                      color: Color(0xff999999),
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w700)),
+                            ),
+                            DataColumn(
+                              label: Text('역할',
+                                  style: TextStyle(
+                                      color: Color(0xff999999),
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w700)),
+                            ),
+                          ],
+                          rows: [
+                            for (var i in moviesData?['companys'])
+                              DataRow(
+                                cells: [
+                                  DataCell(Text(i['companyNm'],
+                                      style: TextStyle(
+                                          color: Color(0xff999999),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w700),
+                                      textAlign: TextAlign.right)),
+                                  DataCell(Text(i['companyPartNm'],
+                                      style: TextStyle(
+                                          color: Color(0xff999999),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w700),
+                                      textAlign: TextAlign.right))
+                                ],
+                              ),
+                          ],
+                        ),
                       ),
-                     
-                    ],
-                  ),
-                ),
+                      AccordionSection(
+                        isOpen: false,
+                        leftIcon: const Icon(Icons.person, color: Colors.white),
+                        header: Text('배우',
+                            style: TextStyle(
+                                color: Color(0xffffffff),
+                                fontSize: 15,233213232
 
-                AccordionSection(
-                  isOpen: false,
-                  leftIcon: const Icon(Icons.person, color: Colors.white),
-                  header: Text('배우', style: _headerStyle),
-                  content: DataTable(
-                    sortAscending: true,
-                    sortColumnIndex: 1,
-                    dataRowHeight: 40,
-                    showBottomBorder: false,
-                    columns: [
-                      DataColumn(
-                          label: Text('배역', style: _contentStyleHeader),),
-                      DataColumn(
-                          label: Text('배우 이름', style: _contentStyleHeader)),
-                    ],
-                    rows: [
-                      for (var i in moviesData?['actors'])
-                      DataRow(
-                        cells: [
-                          DataCell(Text(i['cast'],
-                              style: _contentStyle)),
-                          DataCell(Text(i['peopleNm'], style: _contentStyle)),
-                        ],
+
+                                
+                                fontWeight: FontWeight.bold)),
+                        content: DataTable(
+                          sortAscending: true,
+                          sortColumnIndex: 1,
+                          dataRowHeight: 40,
+                          showBottomBorder: false,
+                          columns: [
+                            DataColumn(
+                              label: Text('배역',
+                                  style: TextStyle(
+                                      color: Color(0xff999999),
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w700)),
+                            ),
+                            DataColumn(
+                                label: Text('배우 이름',
+                                    style: TextStyle(
+                                        color: Color(0xff999999),
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.w700))),
+                          ],
+                          rows: [
+                            for (var i in moviesData?['actors'])
+                              DataRow(
+                                cells: [
+                                  DataCell(Text(i['cast'],
+                                      style: TextStyle(
+                                          color: Color(0xff999999),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w700))),
+                                  DataCell(Text(i['peopleNm'],
+                                      style: TextStyle(
+                                          color: Color(0xff999999),
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w700))),
+                                ],
+                              ),
+                          ],
+                        ),
                       ),
-                     
                     ],
                   ),
-                ),
-                
-          ],
-        ),
-              ],
-            );
-          } else {
-            return Center(child: CircularProgressIndicator(color: Colors.pink,));
-          }
-        }
-      ),
+                ],
+              );
+            } else {
+              return Center(
+                  child: CircularProgressIndicator(
+                color: Colors.pink,
+              ));
+            }
+          }),
     );
   }
 }
